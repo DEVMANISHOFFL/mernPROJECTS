d34e87f12000fd396ede63530f8083b534446111
Manish Dewangan <142229593+DEVMANISHOFFL@users.noreply.github.com>
2024-05-23 15:33:48 +0000

    added Authentication and course module

--- course-selling-app/backend.js
+++ course-selling-app/backend.js
@@ -1,66 +1,115 @@
 const express = require('express');
 const app = express();
 const port = 3000;
+const jwt = require('jsonwebtoken');
+const mongoose = require('mongoose');
 
-const ADMINS = [];
-const USERS = [];
-const COURSES = [];
+const { allowedNodeEnvironmentFlags } = require('process');
+const { callbackify } = require('util');
+
+const userSchema = new mongoose.Schema({
+    username: {type: String},
+    password: String,
+    purchasedCourses: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Course' }]
+  });
+  
+  const adminSchema = new mongoose.Schema({
+    username: String,
+    password: String
+  });
+  
+  const courseSchema = new mongoose.Schema({
+    title: String,
+    description: String,
+    price: Number,
+    imageLink: String,
+    published: Boolean
+  });
+
+const User = mongoose.model('User', userSchema);
+const Admin = mongoose.model('Admin', adminSchema);
+const Course = mongoose.model('Course', courseSchema);
+
+
+const secret = "secret";
 
 app.use(express.json());
 
+const adminAuthentication = (req, res, next) => {
+    const authHeader = req.headers.authorization;
+    if(authHeader){
+        const token = authHeader.split(' ')[1];
+        jwt.verify(token,secret, (err, user) => {
+            if(err){
+                return res.sendStatus(403);
+            }
+            req.user = user;
+            next(); 
+        });
+    }else{
+        res.sendStatus(401);
+    }
+};
+
+mongoose.connect('mongodb+srv://manishcodec:v6dBO0oAyl55rUPH@cluster0.7klz06r.mongodb.net/.net/courses', {dbName: "admins" });
+
 app.post("/admin/signup", (req, res) =>{
-    const admin = req.body;
-    const isExist = ADMINS.find(a => a.username === admin.username);
+    const {username, password}  = req.body;
+    function callback(admin){
+        if(admin){
+            res.status(403).json({message:"Admin already exists"});
+        }else{
+            const obj = { username: username, password: password};
+            const newAdmin = new Admin(obj);
+            newAdmin.save();
+            res.json({message: "Admin created successfully"});
+        }
+    }
+    Admin.findOne({username}).then(callback);
+})
 
+app.post("/admin/login", async (req, res) => {
+    const {username, password} = req.headers;
+    const isExist = await Admin.findOne({username, password});
     if(isExist){
-        res.json({message: "Admin already exists"})
+        const token = jwt.sign({username, role: "admin"}, secret);
+        res.status(200).json({message: "Admin Login Successfull", token});
     }else{
-        ADMINS.push(admin);
-        res.status(200).json({message: "Admin created successfully"});
-    }
+        res.status(403).json({message: "Invalid username or password"});
+    } 
 })
 
-app.post("/admin/login", (req, res) => {
-    const admin = req.body;
-    const isExist = ADMINS.find(a => a.username === admin.username && a.password === admin.password);
+app.post("/users/signup", (req, res)=> {
+    const {username, password} = req.body;
+    function callback(user){
+        if(user){
+            res.status(403).json("user already exist");
+        }else{
+            const obj = {username: username, password: password};
+            const newUser = new User(obj);
+            newUser.save();
+            res.status(200).json({message: "User created successfully"});
+        }
+    }
+        User.findOne({username}).then(callback);
+})
+
+app.post('/users/login',async (req, res) =>{
+    const {username, password} = req.headers;
+    const isExist = await User.findOne({username, password});
     if(isExist){
-        res.status(200).json({message: "Admin Login Successfull"});
+        const token = jwt.sign({username, role:"user"}, secret)
+        res.status(200).json({message:"User login successful", token});
     }else{
-        res.status(404).json("Forbidden");
+        res.status(403).json({message:"Invalid username or password"})
     }
 })
 
-app.get("/admins", (req,res) => {
-    res.status(200).json({admins: ADMINS});
-})
-
-app.post("/users/signup", (req, res) =>{
-    const user = req.body;
-    const existingUser = USERS.find(a => a.username === user.username);
-    if(existingUser){
-       res.json({message:"user already exists"});
-    }else{
-        user.id = Date.now();
-        USERS.push(user);
-        res.status(200).json({message: "user created successfully", id: user.id});
-    }
-})
-
-app.post("/users/login", (req, res) => {
-    const user = req.body;
-    const isExist = USERS.find(a => a.username === user.username && a.password === user.password);
-    if(isExist){
-        res.status(200).json({message: "User Login Successful"});
-    }else{
-        res.status(404).json("Forbidden");
-    }
-})
-
-app.get("/users", (req,res) => {
-    const users = USERS.find(a => a.username)
-    res.status(200).json({users: users});
-})
-
+app.post("/admin/courses", adminAuthentication, async (req,res) => {
+    const course = new Course(req.body);
+    await course.save()
+    res.json({message: "Course created successfully", courseId: course.id});
+});
 
 app.listen(port, () =>{
     console.log(`server started at port ${port}`);
